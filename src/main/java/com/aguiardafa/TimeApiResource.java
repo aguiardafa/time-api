85d6db5ec4ebe91f8ee152ad706424bdc3009a12
aguiardafa <diego.fernandes.aguiar@gmail.com>
2026-03-08 01:53:37 -0300

    update TimeApiResource to return current time in JSON format

--- src/main/java/com/aguiardafa/TimeApiResource.java
+++ src/main/java/com/aguiardafa/TimeApiResource.java
@@ -1,16 +1,20 @@
 package com.aguiardafa;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.time.LocalDateTime;
+
 @Path("/time-api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class TimeApiResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return LocalDateTime.now() + "\n";
     }
 }
